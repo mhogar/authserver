9efedb0318fe0965d588d818c00c206309be79f6
Ryan <mhogar98@gmail.com>
2020-07-10 20:50:58 -0400

    #4 - Add database connection setup to server

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"authserver/dependencies"
+	commonhelpers "authserver/helpers/common"
 	"authserver/router"
 	"fmt"
 	"log"
@@ -13,7 +14,20 @@ import (
 func main() {
 	err := config.InitConfig(".")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(commonhelpers.ChainError("error initing config", err))
+	}
+
+	//connect to the database
+	db := dependencies.ResolveDatabase()
+
+	err = db.OpenConnection()
+	if err != nil {
+		log.Fatal(commonhelpers.ChainError("error opening database connection", err))
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(commonhelpers.ChainError("error reaching database", err))
 	}
 
 	//create the server
